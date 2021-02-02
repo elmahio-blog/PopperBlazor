69d78195c8f720e63dd4dac10d199db33a1631c4
KristofferStrube <KristofferStrube@users.noreply.github.com>
2021-02-02 10:14:02 +0000

    deploy: 6a065264bad385d9dbef9b446bb03c6394e276fd

---
+++ js/PopperWrapper.js
@@ -0,0 +1,33 @@
+﻿export function createPopper(reference, popper, options, objRef)
+{
+    options.onFirstUpdate = (state) => {
+        console.log(state);
+        const stateCopy = {
+            placement: state.placement
+        }
+        objRef.invokeMethodAsync('CallOnFirstUpdate', stateCopy)
+    };
+
+    return Popper.createPopper(reference, popper, options);
+}
+
+export function getStateOfInstance(instance) {
+    var state = instance.state;
+    return {
+        placement: state.placement
+    }
+}
+
+export function updateOnInstance(instance) {
+    return instance.update().then(state => ({ placement: state.placement }));
+}
+
+export function setOptionsOnInstance(instance, options, objRef) {
+    options.onFirstUpdate = (state) => {
+        const stateCopy = {
+            placement: state.placement
+        }
+        objRef.invokeMethodAsync('CallOnFirstUpdate', stateCopy)
+    };
+    return instance.setOptions(options).then(state => ({ placement: state.placement }) );
+}
